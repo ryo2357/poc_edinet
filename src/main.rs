0dd2b8d794b1cd4fbdc1a7be5a766c5d0df1f302
ryo2357 <ryosuke.m.ab@gamil.com>
2024-03-31 23:18:38 +0900

    検証開始

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,49 @@ use log::{debug, error};
 async fn main() -> anyhow::Result<()> {
     mylogger::init();
     dotenv::dotenv().ok();
-    // if let Err(r) = running().await {
-    //     error!("{{:?}}:{:?}", r);
-    //     anyhow::bail!("error at running")
-    // };
+    if let Err(r) = test_run().await {
+        error!("{{:?}}:{:?}", r);
+        anyhow::bail!("error at running")
+    };
+    Ok(())
+}
+
+#[allow(dead_code)]
+async fn test_run_2() -> anyhow::Result<()> {
+    use reqwest::Client;
+    // https://api.edinet-fsa.go.jp/api/v2/documents.json?date=yyyy-mm-dd&type=2&Subscription-Key=xxxxx
+    // 日付を指定して、提出文書のリストをダウンロード
+
+    let key = std::env::var("EDINET_API")?;
+    let client = Client::new();
+    let url = "https://api.edinet-fsa.go.jp/api/v2/documents.json";
+    let params = [("date", "2024-03-14"), ("Subscription-Key", &key)];
+    let response = client.get(url).query(&params).send().await?; // 2
+    let body = response.text().await?; // 3
+    println!("{}", body);
+
+    // "metadata": {
+    //     "title": "提出された書類を把握するためのAPI",
+    //     "parameter": {
+    //       "date": "2024-03-14",
+    //       "type": "1"
+    //     },
+    //     "resultset": {
+    //       "count": 351
+    //     },
+    //     "processDateTime": "2024-03-31 00:01",
+    //     "status": "200",
+    //     "message": "OK"
+    //   }
+    // }
+
+    Ok(())
+}
+
+#[allow(dead_code)]
+async fn test_run_1() -> anyhow::Result<()> {
+    // APIキーの取得
+    let key = std::env::var("EDINET_API")?;
+    println!("{:?}", key);
     Ok(())
 }
